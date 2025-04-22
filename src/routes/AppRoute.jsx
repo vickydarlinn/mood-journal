9ffcd93fc36d63868d6484915a4c5546997e6fa5
vickydarlinn <uttapalsangwan@gmail.com>
2025-04-22 19:39:30 +0530

    feat:add two more pages

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/home";
-import Calendar from "../pages/calendar";
+import Settings from "../pages/settings";
 import NotFound from "../pages/not-found";
+import Statistics from "../pages/statistics";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
-        <Route path="/calendar" element={<Calendar />} />
+        <Route path="statistics" element={<Statistics />} />
+        <Route path="settings" element={<Settings />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
